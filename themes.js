7d01687c5b2fb8a08b97eed20b52a5a9ae240869
LiangChaokai <youchoudeyu@qq.com>
2020-11-14 23:19:59 +0800

    fix: 修复该主题的一些问题，以及对该主题进行完善

--- themes.js
+++ themes.js
@@ -39,7 +39,7 @@ export default {
     owner: 'DawnLck',
     repo: 'juejin-markdown-theme-hydrogen',
     path: 'hydrogen.scss',
-    ref: '9352465',
+    ref: '0b9a5f1',
   },
   fancy: {
     owner: 'xrr2016',
