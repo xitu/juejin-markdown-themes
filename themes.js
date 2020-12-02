e70bd62de51bd10cc1a95fd5596bb3399ab9261a
Healer-Gitee <704599298@qq.com>
2020-12-02 10:45:41 +0800

    theme-> healer-readable init

--- themes.js
+++ themes.js
@@ -67,5 +67,12 @@ export default {
     path: 'vue-pro.scss',
     ref: '836e9bc',
     highlight: 'juejin'
+  },
+  'healer-readable': {
+    owner: 'healerLZH',
+    repo: 'juejin-markdown-theme-healer-readable',
+    path: 'healer-readable.scss',
+    ref: 'c35582e',
+    highlight: 'vs2015'
   }
 };
