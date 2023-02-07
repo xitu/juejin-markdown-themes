e825203dd2bfbaf07cf5d037c0d5e577b7784479
董辉 <43157877+blllld@users.noreply.github.com>
2023-02-07 13:28:57 +0800

    fix: 调整主题标题字体大小, comment块和code块整体区域
    1. 一级调整为30 二级调整为24 三级调整为20
    2. code下添加comment块,让代码注释整体性加强.

--- themes.js
+++ themes.js
@@ -158,7 +158,7 @@ const themes = {
     owner:"blllld",
     repo:"juejin-markdown-theme-nico",
     path:"nico.scss",
-    ref:'46326a0',
+    ref:'532e73e',
     highlight:'atelier-sulphurpool-light'
   },
   'devui-blue': {
